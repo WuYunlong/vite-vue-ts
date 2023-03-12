0ad9a72adada5dd4fdf3931a5c4033ee57161ad9
WuYunlong <17771258@qq.com>
2023-03-12 21:55:11 +0800

    4 files modified

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
+import Arco from '@arco-design/web-vue';
+import '@arco-design/web-vue/dist/arco.css';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(Arco);
+app.mount('#app');
